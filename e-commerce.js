859c5773f4b205fbb9335868c65e55e059f70ab6
LeonyMalasanManalili <erlyon15@gmail.com>
2022-05-31 01:51:59 +0200

    implemented 2 solutions for images

--- e-commerce.js
+++ e-commerce.js
@@ -1,3 +1,5 @@
+// Counter
+
 let numero = 0
 const counter = document.getElementById("counter")
 
@@ -12,6 +14,64 @@ let minus = document.getElementById("minus").addEventListener("click",()=>{
         counter.innerHTML=`${numero}`
     }
 })
+
+// Media
+
+let Big = document.getElementById("bigbox")
+
+//SOLUZIONE N1 incomleta di remove
+
+/*
+
+Big.classList.add("big1")
+
+Bigone.addEventListener("click",()=>
+{
+    Big.classList.add("big1")
+})
+
+let Bigsecond = document.getElementById("box2")
+
+Bigsecond.addEventListener("click",()=>{
+    Big.classList.add("big2")
+})
+
+let Bigthird = document.getElementById("box3")
+
+Bigthird.addEventListener("click",()=>{
+    Big.classList.add("big3")
+})
+    
+let Bigfourth = document.getElementById("box4")
+
+Bigfourth.addEventListener("click",()=>{
+    Big.classList.add("big4")
+}) */
+
+//SOLUZIONE 2
+
+let images = [`./assets/img/image-product-1.jpg`,`./assets/img/image-product-2.jpg`,`./assets/img/image-product-3.jpg`,`./assets/img/image-product-4.jpg`]
+
+Big.innerHTML=`<img src="./assets/img/image-product-${1}.jpg" style="width:100%;border-radius: 15px;">`
+
+const box1 = document.getElementById("box1").addEventListener("click",()=>{
+    Big.innerHTML=`<img src="./assets/img/image-product-${1}.jpg" style="width:100%;border-radius: 15px;">`
+})
+
+const box2 = document.getElementById("box2").addEventListener("click",()=>{
+    Big.innerHTML=`<img src="./assets/img/image-product-${2}.jpg" style="width:100%;border-radius: 15px;">`
+})
+
+const box3 = document.getElementById("box3").addEventListener("click",()=>{
+    Big.innerHTML=`<img src="./assets/img/image-product-${3}.jpg" style="width:100%;border-radius: 15px;">`
+})
+
+const box4 = document.getElementById("box4").addEventListener("click",()=>{
+    Big.innerHTML=`<img src="./assets/img/image-product-${4}.jpg" style="width:100%;border-radius: 15px;">`
+})
+
+
+
 
 
 
